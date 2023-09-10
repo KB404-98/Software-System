01a40ee4a018e28afb2a22b35a243ba0df5b1648
kb <kuldipbhatale111@gmail.com>
2023-09-11 00:05:24 +0530

    Program15 modified

--- Program15.c
+++ Program15.c
@@ -1,3 +1,14 @@
+/*
+================================================================================================
+Name : Program15.c
+Author : Kuldip Nivruti Bhatale
+Roll No : MT2023087
+Description : Write a program to display the environmental variable of the user (use environ).
+Date: 28th Aug, 2023.
+================================================================================================
+*/
+
+
 #include <stdio.h>
 #include <unistd.h>
 
