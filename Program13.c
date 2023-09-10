a34047ff862bb8eab75f1f43cb98889ba9e19e78
kb <kuldipbhatale111@gmail.com>
2023-09-11 00:04:26 +0530

    Program13 modified

--- Program13.c
+++ Program13.c
@@ -1,3 +1,15 @@
+/*
+===================================================================================================================
+Name : Program13.c
+Author : Kuldip Nivruti Bhatale
+Roll No : MT2023087
+Description : Write a program to wait for a STDIN for 10 seconds using select. Write a proper print statement to
+verify whether the data is available within 10 seconds or not (check in $man 2 select). 
+Date: 28th Aug, 2023.
+==================================================================================================================
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/select.h>
