9f2129d4815ef03c1a18ac73675bf8d0c4866be0
kb <kuldipbhatale111@gmail.com>
2023-09-10 14:23:34 +0530

    Program2 modified

--- Program2.c
+++ Program2.c
@@ -1,4 +1,14 @@
-
+/*
+============================================================================
+Name : Program2.c
+Author : Kuldip Nivruti Bhatale
+Roll No : MT2023087
+Description : Write a simple program to execute in an infinite loop at the background. Go to /proc directory and
+identify all the process related information in the corresponding proc directory.
+Date: 25th Aug, 2023.
+Output we can get using ls /proc/pid of the current program
+============================================================================
+*/
 
 #include<stdio.h>
 
