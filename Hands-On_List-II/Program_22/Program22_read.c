aaff2f180aaaa9d07dfc1bf1fa8ad52252f7c1e3
KB <kuldipbhatale111@gmail.com>
2023-10-09 18:58:03 +0530

    Program 22 added by KB

---
+++ Hands-On_List-II/Program_22/Program22_read.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/select.h>
+
+int main() {
+    int fd; // File descriptor for the FIFO
+    char buff[256]; // Buffer for reading
+
+    // Open the FIFO for reading
+    fd = open("myFifo", O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    fd_set rfds;
+    struct timeval tv;
+    int retval;
+
+    FD_ZERO(&rfds);
+    FD_SET(fd, &rfds);
+
+    tv.tv_sec = 10;
+    tv.tv_usec = 0;
+
+    retval = select(fd + 1, &rfds, NULL, NULL, &tv);
+
+    if (retval == -1) {
+        perror("select");
+        close(fd);
+        exit(EXIT_FAILURE);
+    } else if (retval == 0) {
+        printf("No data is available for reading yet\n");
+    } else {
+        if (FD_ISSET(fd, &rfds)) {
+            printf("Data is available now\n");
+            ssize_t bytesRead = read(fd, buff, sizeof(buff));
+            if (bytesRead == -1) {
+                perror("read");
+            } else {
+                buff[bytesRead] = '\0';
+                printf("Data from FIFO: %s\n", buff);
+            }
+        }
+    }
+
+    // Close the FIFO
+    close(fd);
+
+    // Do other tasks
+
+    return 0;
+}
+
